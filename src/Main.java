a1295e78ae555b5bb3b902f850769bf87b64a2ce
Bryce <bxg167@case.edu>
2016-11-28 13:20:50 -0500

    Testing git changes.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		//Hello
 		//Example execution using the Student Database:
 		HashMap<String, String> dummySQLDatabase = new HashMap<>();
 		
