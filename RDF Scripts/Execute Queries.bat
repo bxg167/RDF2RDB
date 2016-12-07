36da619205ed6a570df503e4b70848d913b76bb5
Bryce <bxg167@case.edu>
2016-12-07 14:21:10 -0500

    Added the RDF Queries

---
+++ RDF Scripts/Execute Queries.bat
@@ -0,0 +1,9 @@
+:: An example of how to run the scripts. (You will have to change the paths)
+
+start C:\Users\Bryce\Downloads\apache-jena-3.1.0\bat\sparql.bat --data="C:\Users\Bryce\Documents\GitHub\RDF2RDB\asdf.rdf" --query=q1.rq
+pause
+start C:\Users\Bryce\Downloads\apache-jena-3.1.0\bat\sparql.bat --data="C:\Users\Bryce\Documents\GitHub\RDF2RDB\asdf.rdf" --query=q2.rq
+pause
+start C:\Users\Bryce\Downloads\apache-jena-3.1.0\bat\sparql.bat --data="C:\Users\Bryce\Documents\GitHub\RDF2RDB\asdf.rdf" --query=q3.rq
+pause
+start C:\Users\Bryce\Downloads\apache-jena-3.1.0\bat\sparql.bat --data="C:\Users\Bryce\Documents\GitHub\RDF2RDB\asdf.rdf" --query=q4.rq
