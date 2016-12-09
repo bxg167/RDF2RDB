4677d8bafa62ccb354f041ea13c594bea3d9e4c1
Bryce <bxg167@case.edu>
2016-12-09 10:38:55 -0500

    Edited the query execution batch script

--- RDF Scripts/Execute Queries.bat
+++ RDF Scripts/Execute Queries.bat
@@ -1,4 +1,4 @@
-:: An example of how to run the scripts. (You will have to change the paths)
+::An example of how to query the database. You will have to change the paths to match where Jena and the rdf datastore are located
 
 start C:\Users\Bryce\Downloads\apache-jena-3.1.0\bat\sparql.bat --data="C:\Users\Bryce\Documents\GitHub\RDF2RDB\asdf.rdf" --query=q1.rq
 pause
